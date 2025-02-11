18e6b4d4f63f731026f9098bc68cffdd58846e58
绘星tsuki <xiaoyuesun915@gmail.com>
2025-02-11 20:45:47 +0800

    [change] 更改应用图标
    Signed-off-by: 绘星tsuki <xiaoyuesun915@gmail.com>

--- Scripts/Player/Player.cs
+++ Scripts/Player/Player.cs
@@ -1,12 +1,19 @@
-using System.ComponentModel;
 using Godot;
 
-namespace Zelda2D.Scripts;
+namespace Zelda2D.Scripts.Player;
 
 public partial class Player : CharacterBody2D
 {
-    [ExportGroup("玩家属性")] [Export]
-    public float Speed;
+    [ExportGroup("玩家属性")] [Export] public float Speed;
+
+    private PlayerAnimationController _animationController;
+
+    public override void _Ready()
+    {
+        // 获取动画控制器
+        _animationController =
+            GetNode<PlayerAnimationController>("AnimatedSprite2D");
+    }
 
     public override void _PhysicsProcess(double delta)
     {
@@ -15,18 +22,15 @@ public partial class Player : CharacterBody2D
 
     private void Move(float delta)
     {
+        // 获取输入方向
         Vector2 direction =
             Input.GetVector("MoveLeft", "MoveRight", "MoveUp", "MoveDown");
         // 有输入时移动
         if (direction != Vector2.Zero)
-        {
             Velocity = Speed * delta * direction;
-        }
         // 没有输入时减速
         else
-        {
             Velocity = Velocity.MoveToward(Vector2.Zero, Speed * delta);
-        }
 
         MoveAndSlide();
     }
---
+++ Scripts/Player/PlayerAnimationController.cs
@@ -0,0 +1,14 @@
+using Godot;
+
+namespace Zelda2D.Scripts.Player;
+
+public partial class PlayerAnimationController : AnimatedSprite2D
+{
+	public override void _Ready()
+	{
+	}
+	
+	public override void _Process(double delta)
+	{
+	}
+}
